1d88dfe9ee450a5755eacb571599791d3e7de245
Adrien Samson <adrien@kyklydse.com>
2012-10-23 21:03:39 +0200

    Allow choosing owner when creating a list

--- src/Kyklydse/ChristmasListBundle/Form/ListType.php
+++ src/Kyklydse/ChristmasListBundle/Form/ListType.php
@@ -2,6 +2,8 @@
 
 namespace Kyklydse\ChristmasListBundle\Form;
 
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -9,7 +11,14 @@ class ListType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', 'text', array('label' => 'List name'));
+        $builder
+            ->add('owner', null, array('label' => 'Owner'))
+            ->add('name', null, array('label' => 'List name'));
+    }
+    
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array('data_class' => 'Kyklydse\\ChristmasListBundle\\Document\\ChristmasList'));
     }
     
     public function getName()
