5b17393d6944913b9ebbb56f6e28509035ed57ca
Adrien Samson <adrien@kyklydse.com>
2013-06-13 19:27:55 +0200

    Upgrade to Sf2.3.1

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,7 +8,9 @@ require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
+Debug::enable();
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 $kernel->handle(Request::createFromGlobals())->send();
